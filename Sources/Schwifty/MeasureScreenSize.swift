40cc5b23bbee385443d92c3024f4068ca7631b7f
Federico Curzel <federico@curzel.it>
2022-11-03 11:16:10 +0100

    Added utility to measure screen size on macos

--- Sources/Schwifty/MeasureScreenSize.swift
+++ Sources/Schwifty/MeasureScreenSize.swift
@@ -9,6 +9,41 @@ extension View {
     }
 }
 
+#if os(macOS)
+private struct MeasurementView: NSViewControllerRepresentable {
+    typealias NSViewControllerType = MeasurementViewController
+    let viewController: MeasurementViewController
+    
+    init(size: Binding<CGSize>) {
+        viewController = MeasurementViewController(size: size)
+    }
+    
+    func makeNSViewController(context: Context) -> NSViewControllerType {
+        viewController
+    }
+    
+    func updateNSViewController(_ uiViewController: NSViewControllerType, context: Context) {
+        // ...
+    }
+}
+
+private class MeasurementViewController: NSViewController {
+    @Binding var size: CGSize
+    
+    init(size: Binding<CGSize>) {
+        self._size = size
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { abort() }
+    
+    
+    override func viewWillTransition(to newSize: NSSize) {
+        super.viewWillTransition(to: newSize)
+        size = newSize
+    }
+}
+#else
 private struct MeasurementView: UIViewControllerRepresentable {
     typealias UIViewControllerType = MeasurementViewController
     let viewController: MeasurementViewController
@@ -17,11 +52,11 @@ private struct MeasurementView: UIViewControllerRepresentable {
         viewController = MeasurementViewController(size: size)
     }
     
-    func makeUIViewController(context: Context) -> MeasurementViewController {
+    func makeUIViewController(context: Context) -> UIViewControllerType {
         viewController
     }
     
-    func updateUIViewController(_ uiViewController: MeasurementViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         // ...
     }
 }
@@ -37,6 +72,8 @@ private class MeasurementViewController: UIViewController {
     required init?(coder: NSCoder) { abort() }
     
     override func viewWillTransition(to newSize: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: newSize, with: coordinator)
         size = newSize
     }
 }
+#endif
