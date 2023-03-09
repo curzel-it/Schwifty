1a5c6b46c10d9794769b9c6ca4c2b59f7d54d529
Federico Curzel <federico@curzel.it>
2023-03-09 07:54:45 +0100

    Added wrappers for NSView and UIView, SomeView, and for NSWindow and UIWindow, SomeWindow

--- Sources/Schwifty/OnWindow.swift
+++ Sources/Schwifty/OnWindow.swift
@@ -1,49 +1,49 @@
 import SwiftUI
 
+public extension View {
+    func onWindow(_ foo: @escaping (SomeWindow) -> Void) -> some View {
+        background(WindowGrabber(onWindow: foo))
+    }
+}
+
 #if os(macOS)
-    public extension View {
-        func onWindow(_ foo: @escaping (NSWindow) -> Void) -> some View {
-            background(WindowGrabber(onWindow: foo))
-        }
-    }
+public typealias SomeWindow = NSWindow
+public typealias SomeView = NSView
 
-    private struct WindowGrabber: NSViewRepresentable {
-        let onWindow: (NSWindow) -> Void
-
-        func makeNSView(context: Context) -> NSView {
-            let view = NSView()
-            view.layer?.backgroundColor = .clear
-            DispatchQueue.main.async {
-                if let window = view.window {
-                    self.onWindow(window)
-                }
+private struct WindowGrabber: NSViewRepresentable {
+    let onWindow: (SomeWindow) -> Void
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        view.layer?.backgroundColor = .clear
+        DispatchQueue.main.async {
+            if let window = view.window {
+                self.onWindow(window)
             }
-            return view
         }
-
-        func updateNSView(_ view: NSView, context: Context) {}
+        return view
     }
+    
+    func updateNSView(_ view: NSView, context: Context) {}
+}
 #else
-    public extension View {
-        func onWindow(_ foo: @escaping (UIWindow) -> Void) -> some View {
-            background(WindowGrabber(onWindow: foo))
-        }
-    }
+public typealias SomeWindow = UIWindow
+public typealias SomeView = UIView
 
-    private struct WindowGrabber: UIViewRepresentable {
-        let onWindow: (UIWindow) -> Void
-
-        func makeUIView(context: Context) -> UIView {
-            let view = UIView()
-            view.backgroundColor = .clear
-            DispatchQueue.main.async {
-                if let window = view.window {
-                    self.onWindow(window)
-                }
+private struct WindowGrabber: UIViewRepresentable {
+    let onWindow: (SomeWindow) -> Void
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        DispatchQueue.main.async {
+            if let window = view.window {
+                self.onWindow(window)
             }
-            return view
         }
-
-        func updateUIView(_ view: UIView, context: Context) {}
+        return view
     }
+    
+    func updateUIView(_ view: UIView, context: Context) {}
+}
 #endif
