9620d6a851f4c81c458cfa37fe50d0906f872a9c
Federico Curzel <federico@curzel.it>
2024-03-10 16:30:50 +0100

    More array-split options

--- Sources/Schwifty/SequencesChunking.swift
+++ Sources/Schwifty/SequencesChunking.swift
@@ -17,15 +17,30 @@ public extension Array {
 }
 
 public extension Sequence {
-    func split(count parts: Int) -> [[Element]] {
-        Array(self).split(count: parts)
+    func split(numberOfParts parts: Int) -> [[Element]] {
+        Array(self).split(numberOfParts: parts)
     }
 }
 
 public extension Array {
-    func split(count parts: Int) -> [[Element]] {
-        stride(from: 0, to: count, by: parts).map {
-            Array(self[$0 ..< Swift.min($0 + parts, count)])
+    func split(numberOfParts parts: Int) -> [[Element]] {
+        guard parts > 0 else { return [] }
+        let chunkSize = self.count / parts
+        let extraElements = self.count % parts
+        var result: [[Element]] = []
+        var startIndex = 0
+        
+        for chunkIndex in 0..<parts {
+            var endIndex = startIndex + chunkSize
+            if chunkIndex < extraElements {
+                endIndex += 1
+            }
+            if endIndex > self.count { endIndex = self.count }
+            let chunk = Array(self[startIndex..<endIndex])
+            result.append(chunk)
+            startIndex = endIndex
         }
+        
+        return result
     }
 }
--- Tests/SchwiftyTests/ArraySlicingTests.swift
+++ Tests/SchwiftyTests/ArraySlicingTests.swift
@@ -1,8 +1,20 @@
-//
-//  File.swift
-//  
-//
-//  Created by Federico Curzel on 10/03/24.
-//
+import XCTest
 
-import Foundation
+@testable import Schwifty
+
+class ArraySlicingTests: XCTestCase {
+    func testCanChunkArray() {
+        XCTAssertEqual([Int]().chunks(ofSize: 3), [])
+        XCTAssertEqual([1,2,3].chunks(ofSize: 3), [[1,2,3]])
+        XCTAssertEqual([1,2,3].chunks(ofSize: 1), [[1],[2],[3]])
+        XCTAssertEqual([1,2,3].chunks(ofSize: 2), [[1,2],[3]])
+        XCTAssertEqual([1,2,3,4].chunks(ofSize: 3), [[1,2,3],[4]])
+    }
+    
+    func testCanSplitArray() {
+        XCTAssertEqual([1,2,3].split(numberOfParts: 0), [])
+        XCTAssertEqual([1,2,3].split(numberOfParts: 2), [[1,2], [3]])
+        XCTAssertEqual([1,2,3,4].split(numberOfParts: 2), [[1,2], [3,4]])
+        XCTAssertEqual([1,2,3,4].split(numberOfParts: 4), [[1],[2],[3],[4]])
+    }
+}
